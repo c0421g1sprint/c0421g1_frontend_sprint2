ce1b04b099e89994360da9500f4a5c6a48d73646
c0421g1sprint <83199138+anhquan110297@users.noreply.github.com>
2021-11-19 22:36:11 +0700

    QuanTA update code front end

--- src/app/feature-module/feature.module.ts
+++ src/app/feature-module/feature.module.ts
@@ -1,26 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AccountModule } from './account/account.module';
-import { EmployeeModule } from './employee/employee.module';
-import { FeedbackModule } from './feedback/feedback.module';
-import { FoodAndDrinkModule } from './food-and-drink/food-and-drink.module';
-import { OrderModule } from './order/order.module';
-import { TableModule } from './table/table.module';
-import { HomepageBodyComponent } from './homepage-body/homepage-body.component';
-import { HomepageModule } from './homepage-body/homepage.module';
-import {ShareModule} from "../share-module/share.module";
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AccountModule} from './account/account.module';
-import {ShareModule} from '../share-module/share.module';
-import {EmployeeModule} from './employee/employee.module';
-import {FeedbackModule} from './feedback/feedback.module';
-import {FoodAndDrinkModule} from './food-and-drink/food-and-drink.module';
-import {OrderModule} from './order/order.module';
-import {TableModule} from './table/table.module';
-import {FeedbackComponent} from './feedback/feedback.component';
 import {HomepageBodyComponent} from "./homepage-body/homepage-body.component";
+import {AccountModule} from "./account/account.module";
+import {CommonModule} from "@angular/common";
+import {EmployeeModule} from "./employee/employee.module";
+import {FeedbackModule} from "./feedback/feedback.module";
+import {FoodAndDrinkModule} from "./food-and-drink/food-and-drink.module";
+import {OrderModule} from "./order/order.module";
+import {TableModule} from "./table/table.module";
 import {HomepageModule} from "./homepage-body/homepage.module";
+import {ShareModule} from "../share-module/share.module";
+import {NgModule} from "@angular/core";
 
 
 @NgModule({
