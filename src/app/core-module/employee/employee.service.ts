453ce3b1e72cb8553c67649b0d33e7c2230bd00a
myusername <nhatdv510@wru.vn>
2021-11-21 22:18:46 +0700

    commit 21/11

--- src/app/core-module/employee/employee.service.ts
+++ src/app/core-module/employee/employee.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
+import {IEmployee} from "../../entity/IEmployee";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+  // xem thông tin user by Nhật
+  getUserDetail(userDetail:any): Observable<IEmployee|any>{
+    return this.http.get('http://localhost:8080/api/employee/userDetail?name='+userDetail)
+  }
 }
