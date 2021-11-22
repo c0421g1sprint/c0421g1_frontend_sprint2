2aeb0b09425e34ef2459ee0aab6c1364e282cf0a
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-11-22 22:59:51 +0700

    hien thi category - LinhDN 22/11

--- src/app/feature-module/category/list-category/list-category.component.ts
+++ src/app/feature-module/category/list-category/list-category.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ICategory} from "../../../entity/ICategory";
 import {CategoryService} from "../../../core-module/food_and_drink/category.service";
 import {SnackbarService} from "../../../core-module/snackbar/snackbar.service";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogDeleteComponent} from "../../../share-module/dialog-delete/dialog-delete.component";
 
 @Component({
   selector: 'app-list-category',
@@ -16,13 +18,14 @@ export class ListCategoryComponent implements OnInit {
   code: string = '';
   oldName: string = '';
   oldCode: string = '';
-  pageObj: any = {page: 0, size: 10}
+  pageObj: any = {page: 0, size: 5}
   responsePage: any; //tạo biến để nhận giá trị Observable
   totalPages: number = 0;
   totalElement: number = 0;
 
   constructor(private categoryService: CategoryService,
-              private snackBar: SnackbarService) { }
+              private snackBar: SnackbarService,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.viewAllCategory(this.pageObj);
@@ -31,6 +34,7 @@ export class ListCategoryComponent implements OnInit {
   //hien thi danh sach category
   viewAllCategory(pageObj: any){
     if ((this.code||this.name)!=""){
+      this.pageObj.page = 0;
       if ((this.code==this.oldCode&&this.name==this.oldName)){
         this.pageObj.page = 0;
         this.oldName = this.name;
@@ -38,9 +42,11 @@ export class ListCategoryComponent implements OnInit {
       }
     }
     let name = this.name.trim();
-    this.categoryService.viewAllCategory(pageObj,this.code,name).subscribe(data =>{
+    let code = this.code.trim();
+    this.categoryService.viewAllCategory(pageObj,code,name).subscribe(data =>{
       console.log(data);
       this.responsePage = data;
+      console.log(this.responsePage);
       this.categoryList = this.responsePage.content;
       this.totalPages = this.responsePage.totalPages;
       this.totalElement = this.responsePage.totalElement;
@@ -63,6 +69,53 @@ export class ListCategoryComponent implements OnInit {
   }
 
   openDialogDelete(categoryName: string, categoryId: number) {
+    let dialog =this.dialog.open(DialogDeleteComponent, {
+      data: {
+        id: categoryId,
+        name: categoryName,
+        object: "nhóm món"
+      }
+    })
+  dialog.afterClosed().subscribe(nextClose=>{
+    if (nextClose == `true`) {
+      this.categoryService.delete(categoryId, this.category).subscribe(data => {
+        console.log(data);
+        this.snackBar.showSnackbar("Xoá nhóm món " + name + " thành công", "success");
+        this.ngOnInit();
+      })
+    }
+  })
+  }
 
+  previousPage() {
+    this.pageObj.page--;
+    if (this.pageObj.page<=0){
+      this.pageObj.page = 0;
+    }
+    console.log(this.pageObj)
+    this.viewAllCategory(this.pageObj)
+  }
+
+  nextPage() {
+    console.log(this.totalPages)
+    this.pageObj.page++;
+    if (this.pageObj.page>=this.responsePage.totalPages-1){
+      this.pageObj.page = this.responsePage.totalPages-1 ;
+    }
+    console.log(this.pageObj)
+    this.viewAllCategory(this.pageObj)
+  }
+
+  getPage(value: string) {
+    if (value == null) {
+      this.snackBar.showSnackbar("Vui lòng nhập số trang cần tìm", 'error');
+    }
+    if (Number(value) <= this.responsePage.totalPages && Number(value) > 0 && Number(value) % 1 == 0) {
+      this.pageObj['page'] = Number(value) - 1
+      console.log(this.pageObj['page'])
+      this.viewAllCategory(this.pageObj);
+    } else {
+      this.snackBar.showSnackbar("Trang cần tìm không hợp lệ", 'error');
+    }
   }
 }
