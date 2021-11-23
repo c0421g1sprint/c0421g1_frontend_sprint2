04e680b230a208c39513c078ff22c05e6e3e613a
myusername <nhatdv510@wru.vn>
2021-11-23 14:25:20 +0700

    up

---
+++ src/app/entity/IEditAccount.ts
@@ -0,0 +1,9 @@
+export interface IEditAccount {
+  accountId: number;
+  accountUsername: string;
+  accountPassword: string;
+  email: string;
+  is_active: boolean;
+  is_not_block: boolean;
+  is_delete: boolean;
+}
