df5c4f78f7d15f0d5a91c4e7af8eaf63df616fca
PC <minhngo171997@gmail.com>
2021-11-25 16:51:29 +0700

    Minh update 25/11

--- src/app/core-module/employee/employee.service.ts
+++ src/app/core-module/employee/employee.service.ts
@@ -12,18 +12,22 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
+  //MinhNN
   getAllEmployee(page: number): Observable<IEmployee[] | any> {
     return this.http.get(this.url + "list" + "?page=" + page);
   }
 
+  //MinhNN
   findById(id: number): Observable<IEmployee | any> {
     return this.http.get(this.url + id);
   }
 
+  //MinhNN
   deleteEmployee(id: number, iEmployee: IEmployee): Observable<IEmployee|any> {
     return this.http.patch(this.url + "delete/" + id, iEmployee);
   }
 
+  //MinhNN
   search(page: number, username: String, nameEmployee: String, phone: String): Observable<IEmployee[]|any> {
     return this.http.get(this.url + "search" + "?page=" + page + "&username=" + username + "&nameEmployee=" + nameEmployee + "&phone=" + phone);
   }
