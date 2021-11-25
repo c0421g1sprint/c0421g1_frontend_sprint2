34ea8cc1cf50d777a4368ebed1e196e03a9e0633
kytai3003 <tai.hvk@gmail.com>
2021-11-26 01:05:26 +0700

    TaiHVK commit 26/11

--- src/app/feature-module/account/account-routing.module.ts
+++ src/app/feature-module/account/account-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {AccountComponent} from "./account.component";
 
-const routes: Routes = [
-  {path: 'login', component: AccountComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [
--- src/app/feature-module/order/order-routing.module.ts
+++ src/app/feature-module/order/order-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {OrderOnServiceComponent} from "./order-on-service/order-on-service.component";
+import {OrderComponent} from "./order.component";
 
 const routes: Routes = [
-  {path: "sale", component: OrderOnServiceComponent}
+  {path: "order/sale", component: OrderOnServiceComponent}
 ];
 
 @NgModule({
