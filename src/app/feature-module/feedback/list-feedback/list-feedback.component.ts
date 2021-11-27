5e1f421d550b7de7abf6333a3ba7b376b159f824
c0421g1sprint <83199138+anhquan110297@users.noreply.github.com>
2021-11-27 17:53:18 +0700

    QuanTA update FE 27/11 feedback list

--- src/app/feature-module/feedback/list-feedback/list-feedback.component.ts
+++ src/app/feature-module/feedback/list-feedback/list-feedback.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FeedbackService} from "../../../core-module/feedback/feedback.service";
 import {MatDialog} from "@angular/material/dialog";
 import {IFeedback} from "../../../entity/IFeedback";
 import {DialogDetailComponent} from "../dialog-detail/dialog-detail.component";
 import {SnackbarService} from "../../../core-module/snackbar/snackbar.service";
-import { DatePipe } from '@angular/common';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-list-feedback',
@@ -13,16 +13,16 @@ import { DatePipe } from '@angular/common';
   providers: [DatePipe]
 })
 export class ListFeedbackComponent implements OnInit {
-  feedBackList : IFeedback[];
-  currentPage : number = 0;
+  feedBackList: IFeedback[];
+  currentPage: number = 0;
   messError = '';
   inputDate = null;
-  totalPages :number = 0;
+  totalPages: number = 0;
   myDate: any = new Date();
 
-  constructor(private feedbackService : FeedbackService,
-              private matDialog : MatDialog,
-              private snackBar : SnackbarService,
+  constructor(private feedbackService: FeedbackService,
+              private matDialog: MatDialog,
+              private snackBar: SnackbarService,
               private datePipe: DatePipe) {
   }
 
@@ -31,19 +31,20 @@ export class ListFeedbackComponent implements OnInit {
   }
 
   // method get list
-  findAllFeedBackList(){
+  findAllFeedBackList() {
     this.myDate = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
-    this.feedbackService.findAllFeedBack(this.inputDate,this.currentPage).subscribe(next =>{
+    this.feedbackService.findAllFeedBack(this.inputDate, this.currentPage).subscribe(next => {
       console.log(next.totalPages);
       this.totalPages = next.totalPages;
       this.feedBackList = next.content;
       console.log(this.feedBackList)
     }, error => {
-      if (error.status == '404'){
+      if (error.status == '404') {
         this.messError = 'Hiện tại không có phản hồi của khách hàng'
       }
     })
   }
+
   // lui trang
   previousPage() {
     console.log(this.currentPage);
@@ -52,6 +53,7 @@ export class ListFeedbackComponent implements OnInit {
       this.ngOnInit();
     }
   }
+
   // sang trang
   nextPage() {
     console.log(this.currentPage);
@@ -60,39 +62,42 @@ export class ListFeedbackComponent implements OnInit {
       this.ngOnInit();
     }
   }
+
   // search list by date
   searchByDate(value: string) {
-      this.inputDate = value;
-      if (this.inputDate == ""){
-        this.inputDate = null
+    this.inputDate = value;
+    if (this.inputDate == "") {
+      this.inputDate = null
+    }
+    console.log(this.inputDate)
+    this.feedbackService.findAllFeedBack(this.inputDate, this.currentPage).subscribe(next => {
+      this.feedBackList = next.content;
+      console.log(this.feedBackList)
+    }, error => {
+      if (error.status == '404') {
+        this.snackBar.showSnackbar('Không có phản hồi cho ngày được chọn', 'error')
       }
-      console.log(this.inputDate)
-      this.feedbackService.findAllFeedBack(this.inputDate,this.currentPage).subscribe(next =>{
-        this.feedBackList = next.content;
-        console.log(this.feedBackList)
-      }, error => {
-        if (error.status == '404'){
-          this.feedBackList = [];
-        }
-      })
-  }
-  // open dialog
-  openDialog(id: number) {
-    let dialogRef = this.matDialog.open(DialogDetailComponent, {data : id,
-      minHeight : "60%",
-      width : "35%",
-      panelClass : 'custom-dialog-container'
     })
   }
+
+  // open dialog
+  openDialog(id: number) {
+    let dialogRef = this.matDialog.open(DialogDetailComponent, {
+      data: id,
+      minHeight: "60%",
+      width: "35%",
+      panelClass: 'custom-dialog-container'
+    })
+  }
+
   // nhay trang
   jumpingPage(value: string) {
-    if (value == ''){
-      this.snackBar.showSnackbar('Vui lòng nhập số trang','error')
-    }
     let numberValue = +value;
-    if (numberValue > 0 && numberValue <= this.totalPages){
-      this.currentPage = numberValue-1;
+    if (numberValue > 0 && numberValue <= this.totalPages) {
+      this.currentPage = numberValue - 1;
       this.ngOnInit();
+    } else {
+      this.snackBar.showSnackbar('Vui lòng nhập số trang hợp lệ','error')
     }
   }
 }
