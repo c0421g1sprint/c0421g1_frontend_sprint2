82d937066f5ba71221177adcfe97606c8f221cc4
manhdung98na <md0949470893@gmail.com>
2021-11-27 17:52:05 +0700

    DungNM 27/11 - Thêm httpOption vào các request để authen bên Back-end

--- src/app/core-module/food_and_drink/category.service.ts
+++ src/app/core-module/food_and_drink/category.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {ICategory} from "../../entity/ICategory";
+import {StorageService} from "../account/storage.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,50 +11,55 @@ export class CategoryService {
   private urlCategory = "http://localhost:8080/api/category/menu";
   private url = 'http://localhost:8080/api/category';
 
-  constructor(private http: HttpClient) {
-
+  private httpOptions;
+  constructor(private http: HttpClient, private storageService: StorageService) {
+    this.httpOptions ={
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': 'DUNG ' + `${this.storageService.getToken()}`}),
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
   }
 
   getAll(): Observable<ICategory | any> {  //BaoHG
-    return this.http.get(this.urlCategory);
+    return this.http.get(this.urlCategory, this.httpOptions);
   }
 
   //LinhDN hien thi danh sach category
   viewAllCategory(pageObj: any, code: String, name: String): Observable<ICategory[]|any>{
-    return this.http.get(`${this.url}/list?page=${pageObj.page}&size=${pageObj.size}&code=${code}&name=${name}`);
+    return this.http.get(`${this.url}/list?page=${pageObj.page}&size=${pageObj.size}&code=${code}&name=${name}`, this.httpOptions);
   }
   //LinhDN hien thi danh sach category khong co tham so
   viewAllCategoryNoAgrument(): Observable<ICategory[]|any>{
-    return this.http.get(`${this.url}/listFull`);
+    return this.http.get(`${this.url}/listFull`, this.httpOptions);
   }
 
   //LinhDN xoa 1 category
   delete(id: number, category: ICategory): Observable<ICategory | any> {
-    return this.http.patch(`${this.url}/delete/${id}`, category);
+    return this.http.patch(`${this.url}/delete/${id}`, category, this.httpOptions);
   }
 
   //xem 1 category
   detail(id: number): Observable<ICategory | any> {
-    return this.http.get(`${this.url}/detail/${id}`);
+    return this.http.get(`${this.url}/detail/${id}`, this.httpOptions);
   }
 
   //LamNT create
   create(newCategory: ICategory): Observable<ICategory | any> {
-    return this.http.post(this.url + '/create', newCategory);
+    return this.http.post(this.url + '/create', newCategory, this.httpOptions);
   }
 
   //LamNT update
   update(updateCategory: ICategory): Observable<ICategory | any> {
-    return this.http.patch(this.url + '/update', updateCategory);
+    return this.http.patch(this.url + '/update', updateCategory, this.httpOptions);
   }
 
   //LamNT findAllCategory
   findAllCategory(): Observable<ICategory[] | any> {
-    return this.http.get(this.url + '/find-all');
+    return this.http.get(this.url + '/find-all', this.httpOptions);
   }
 
   //LamNT findById
   findCategoryById(id: number): Observable<ICategory | any> {
-    return this.http.get(this.url + '/detail/' + id);
+    return this.http.get(this.url + '/detail/' + id, this.httpOptions);
   }
 }
--- src/app/core-module/table/table.service.ts
+++ src/app/core-module/table/table.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ITables} from "../../entity/ITables";
 import {Observable} from "rxjs";
+import {StorageService} from "../account/storage.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,47 +11,57 @@ export class TableService {
   private urlTable = "http://localhost:8080/api/order/table"; //BaoHG
   private api_table = 'http://localhost:8080/api/table';
 
-  constructor(private http: HttpClient) {
+  private httpOptions;
+
+  constructor(private http: HttpClient, private storageService: StorageService) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'DUNG ' + `${this.storageService.getToken()}`
+      }),
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
   }
 
   randomTableNull(): Observable<ITables | any> { //BaoHG
-    return this.http.get(this.urlTable);
+    return this.http.get(this.urlTable, this.httpOptions);
   }
 
   //HauPT do at 19/11/2021
   getListTables(currentPage: number): Observable<ITables[] | any> {
-    return this.http.get(this.api_table + '/list?page=' + currentPage);
+    return this.http.get(this.api_table + '/list?page=' + currentPage, this.httpOptions);
   }
 
   //HauPT do at 19/11/2021
   searchTables(currentPage: number, tableCode: string, tableStatus: string): Observable<ITables[] | any> {
     if (tableCode == '' && tableStatus == '') {
-      return this.http.get(this.api_table + '/list?page=' + currentPage);
+      return this.http.get(this.api_table + '/list?page=' + currentPage, this.httpOptions);
     }
     if (tableStatus == '') {
-      return this.http.get(this.api_table + '/search?tableCode=' + tableCode + '&page=' + currentPage);
+      return this.http.get(this.api_table + '/search?tableCode=' + tableCode + '&page=' + currentPage, this.httpOptions);
     }
     if (tableCode == '') {
-      return this.http.get(this.api_table + '/search?tableStatus=' + tableStatus + '&page=' + currentPage);
+      return this.http.get(this.api_table + '/search?tableStatus=' + tableStatus + '&page=' + currentPage, this.httpOptions);
     }
-    return this.http.get(this.api_table + '/search?tableCode=' + tableCode + '&tableStatus=' + tableStatus + '&page=' + currentPage);
+    return this.http.get(this.api_table + '/search?tableCode=' + tableCode + '&tableStatus=' + tableStatus + '&page=' + currentPage, this.httpOptions);
   }
 
   //HauPT do at 23/11/2021
   deleteTables(tableId: number): Observable<any> {
-    return this.http.patch(this.api_table + "/delete", tableId);
+    return this.http.patch(this.api_table + "/delete", tableId, this.httpOptions);
   }
 
   //DucLVH do at 24/11/2021
   addWewTables(tables: ITables): Observable<any> {
-    return this.http.post(this.api_table + '/add', tables);
+    return this.http.post(this.api_table + '/add', tables, this.httpOptions);
   }
 
   findByIdTable(id: number): Observable<ITables | any> {
-    return this.http.get(this.api_table + '/' + id);
+    return this.http.get(this.api_table + '/' + id, this.httpOptions);
   }
 
   update(tables: ITables): Observable<ITables | any> {
-    return this.http.patch(this.api_table + '/update', tables);
+    return this.http.patch(this.api_table + '/update', tables, this.httpOptions);
   }
 }
